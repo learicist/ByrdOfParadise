a3674df6a1f77c9159db7122140b38ded1036bb1
The Doctor <learicist@gmail.com>
2020-04-20 17:32:49 -0400

    status effects, et. al

--- main.js
+++ main.js
@@ -1,20 +1,34 @@
 $(document).ready(function () {
 
-	let count, result;
+	let count, 
+		result, 
+		arr6 = [], 
+		arr20 = [];
 	
 	$('#roll6').click(function(){
 		count = 6;
 		result = Math.ceil(Math.random() * count)
-		console.log("You rolled: " + result);
-		$('#result6').html("You rolled: " + result)
+		//console.log("You rolled: " + result);
+		arr6.push(result);
+		$('#result6').html("You have rolled: " + arr6);
 	});
 	
 	$('#roll20').click(function(){
 		count = 20;
 		result = Math.ceil(Math.random() * count)
-		console.log("You rolled: " + result);
-		$('#result20').html("You rolled: " + result)
+		//console.log("You rolled: " + result);
+		arr20.push(result);
+		$('#result20').html("You have rolled: " + arr20);
 	});
 
+	$('#clear6').click(function () {
+		arr6 = [];
+		$('#result6').html('');
+	});
+	
+	$('#clear20').click(function () {
+		arr20 = [];
+		$('#result20').html('');
+	});
 	
 });
